6e354fb5f95c28f4697a3d2ef5fcad646dfe777d
Matt <mattkjames7@gmail.com>
2021-04-28 16:02:15 +0100

    added some stuff to setup.py

---
+++ setup.py
@@ -0,0 +1,58 @@
+import setuptools
+from setuptools.command.install import install
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def getversion():
+	'''
+	read the version string from __init__
+	
+	'''
+	#get the init file path
+	thispath = os.path.abspath(os.path.dirname(__file__))+'/'
+	initfile = thispath + 'JovianModel/__init__.py'
+	
+	#read the file in
+	f = open(initfile,'r')
+	lines = f.readlines()
+	f.close()
+	
+	#search for the version
+	version = 'unknown'
+	for l in lines:
+		if '__version__' in l:
+			s = l.split('=')
+			version = s[-1].strip().strip('"').strip("'")
+			break
+	return version
+	
+version = getversion()
+
+setuptools.setup(
+    name="Jovianfiducialmagneticmodel",
+	version=version,
+    author="Gabby Provan",
+    author_email="?",
+    description="Python module for the CAN 2020 model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabbyprovan/Jovianfiducialmagneticmodel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+    ],
+    install_requires=[
+		'numpy',
+		'scipy',
+		'matplotlib',
+		'DateTimeTools>=0.2.0',
+	],
+	include_package_data=True,
+)
+
+
+
