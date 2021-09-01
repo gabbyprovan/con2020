92a078c28fda73b268305e92040ebbe30280eb8e
mattkjames7 <mattkjames7@gmail.com>
2021-09-01 12:13:38 +0100

    setup.py stuff failed - used just Gabby's details

--- con2020/__init__.py
+++ con2020/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from ._Switcher import _Switcher
 from .Test import Test,TestTimingIntVsAn,TestTimingIntVsAnSingle,TestCompareAnalytic,Dump,TestBessel
