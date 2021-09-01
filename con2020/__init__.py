91c1fb427a8f3b282b7b1ec920e79b94fdd2f6e8
mattkjames7 <mattkjames7@gmail.com>
2021-09-01 13:11:23 +0100

    fixed error in manifest file

--- con2020/__init__.py
+++ con2020/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from ._Switcher import _Switcher
 from .Test import Test,TestTimingIntVsAn,TestTimingIntVsAnSingle,TestCompareAnalytic,Dump,TestBessel
