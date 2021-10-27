c153df467983b419d748f06cbe5d212beb82a31a
mattkjames7 <mattkjames7@gmail.com>
2021-10-27 16:54:10 +0100

    changed xp to 155.8, updated the version number

--- con2020/__init__.py
+++ con2020/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 from ._Switcher import _Switcher
 from .Test import Test,TestTimingIntVsAn,TestTimingIntVsAnSingle,TestCompareAnalytic,Dump,TestBessel
