660d661982faa6aa6c07636ffbb31d52cf0c8507
Matt <mattkjames7@gmail.com>
2022-11-29 13:50:45 +0000

    new minor version

--- con2020/__init__.py
+++ con2020/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 from ._Switcher import _Switcher
 from .Test import Test,TestTimingIntVsAn,TestTimingIntVsAnSingle,TestCompareAnalytic,Dump,TestBessel,PlotLMIC
