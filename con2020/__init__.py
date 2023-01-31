369dcfc1f4be728df24a20c5fb9ef1e24cad8cde
gp31@le.ac.uk <gp31@spectre14.cm.cluster>
2023-01-31 13:52:14 +0000

    new version

--- con2020/__init__.py
+++ con2020/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 from ._Switcher import _Switcher
 from .Test import Test,TestTimingIntVsAn,TestTimingIntVsAnSingle,TestCompareAnalytic,Dump,TestBessel,PlotLMIC
