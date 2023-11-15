c8251f27f7a43a9329af8544cb639f6a433a278e
gabbyprovan <83064572+gabbyprovan@users.noreply.github.com>
2023-11-15 19:25:01 +0000

    Update __init__.py

--- con2020/__init__.py
+++ con2020/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 
 from ._Switcher import _Switcher
 from .Test import Test,TestTimingIntVsAn,TestTimingIntVsAnSingle,TestCompareAnalytic,Dump,TestBessel,PlotLMIC
