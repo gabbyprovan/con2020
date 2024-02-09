d9b0fce9766722c01fb13d67490f7aebc59bc64e
gabbyprovan <83064572+gabbyprovan@users.noreply.github.com>
2024-02-09 11:54:28 +0000

    Update Model.py
    Deleted erroneous print statement

--- con2020/Model.py
+++ con2020/Model.py
@@ -1301,8 +1301,7 @@ class Model(object):
 			
 		#call the model function
 		Brho,Bphi,Bz = self._ModelFunc(rho,abs_z,z)
-			   
-		print('Brho', Brho)   
+			     
 		#return to SIII coordinates
 		B0,B1,B2 = self._OutputConv(cost,sint,cosp,sinp,x,y,rho,Brho,Bphi,Bz)
 		
